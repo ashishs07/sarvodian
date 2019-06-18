92e0b8ac65f32919c3b9165939a4f20a8a3ecf12
Ashish Singh <ashishashoksingh3@gmail.com>
2019-06-19 01:32:30 +0530

    Updated Contact Page

--- lib/pages/contactpage.dart
+++ lib/pages/contactpage.dart
@@ -4,10 +4,28 @@ class ContactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contact Us'),
-      ),
-      body: Text('Contact Us'),
-    );
+        appBar: AppBar(
+          title: Text('Contact Us'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Mobile No +91 84390 65717',
+                  style: TextStyle(fontSize: 22),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Email - thisisjust@demo.email',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
