b40c237468ba67519e67b3fd5c8e2c101c3d3342
Ashish Singh <ashishashoksingh3@gmail.com>
2019-07-05 23:40:52 +0530

    Removed Firebase Temporarily

--- lib/pages/batchtimingspage.dart
+++ lib/pages/batchtimingspage.dart
@@ -4,9 +4,6 @@ class BatchtimingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /* appBar: AppBar(
-        title: Text('Batch Timings'),
-      ),*/
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
--- lib/pages/contactpage.dart
+++ lib/pages/contactpage.dart
@@ -1,48 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_admob/firebase_admob.dart';
-
-//import '../app_id.dart' show appid;
 
 class ContactPage extends StatelessWidget {
-  /* @override
-  State<StatefulWidget> createState() {
-    return _ContactPageState();
-  }
-}
-
-class _ContactPageState extends State<ContactPage> {*/
-  /*static final MobileAdTargetingInfo targetingInfo =
-      MobileAdTargetingInfo(testDevices: appid != null ? [appid] : null);
-
-  BannerAd bannerAd;
-  BannerAd _buildBanner() {
-    return BannerAd(
-        size: AdSize.banner,
-        adUnitId: BannerAd.testAdUnitId,
-        listener: (MobileAdEvent event) {
-          print(event);
-        });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
-    bannerAd = _buildBanner()..load();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    bannerAd?.dispose();
-  }*/
-
   @override
   Widget build(BuildContext context) {
-    //bannerAd
-    //..load()
-    //..show();
     return Scaffold(
         appBar: AppBar(
           title: Text('Contact Us'),
